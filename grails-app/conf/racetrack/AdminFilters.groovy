9343ec6d5a3de44ea931faefb2e186b768610f8b
Jacob Danner <jacob.danner@gmail.com>
2012-08-26 15:46:11 -0700

    added admin only filter - pg135

--- grails-app/conf/racetrack/AdminFilters.groovy
+++ grails-app/conf/racetrack/AdminFilters.groovy
@@ -3,16 +3,18 @@ package racetrack
 class AdminFilters {
 
     def filters = {
-        all(controller:'*', action:'*') {
+      // filter methods for admin only access via regex
+        adminOnly(controller:'*', action:'(create|edit|update|delete|save)') {
+
             before = {
-
+              if(!session?.user?.admin)
+              {
+                flash.message="Sorry, admin only"
+                redirect(controller: "race", action:"list")
+                return false
+              }
             }
-            after = { Map model ->
 
-            }
-            afterView = { Exception e ->
-
-            }
         }
     }
 }
