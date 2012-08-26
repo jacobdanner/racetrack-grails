6662ba4b175da3df1dcadcfac9b89e01b7a0516a
Jacob Danner <jacob.danner@gmail.com>
2012-08-26 15:38:10 -0700

    add empty simple filter, pg134

---
+++ grails-app/conf/racetrack/AdminFilters.groovy
@@ -0,0 +1,18 @@
+package racetrack
+
+class AdminFilters {
+
+    def filters = {
+        all(controller:'*', action:'*') {
+            before = {
+
+            }
+            after = { Map model ->
+
+            }
+            afterView = { Exception e ->
+
+            }
+        }
+    }
+}
